7e15ca43c2e8e6c29e96623e3d214142b612ece2
karreemali <karreem.ali@gmail.com>
2025-04-23 10:30:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 curl "https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Urban_Tree_Canopy/MapServer/4/query?outFields=*&where=1%3D1&f=geojson" --output 2024dccanopy.geojson
+# curl "https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Urban_Tree_Canopy/MapServer/4/query?outFields=*&where=1%3D1&f=geojson" --output 2024dccanopy.geojson
 curl "https://davidmegginson.github.io/ourairports-data/airports.csv" --output airports.csv
